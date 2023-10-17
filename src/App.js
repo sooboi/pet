4bf5de4392110fbb3ace07bd1875850d86e9d178
sooboi <dlgbrkd123@gmail.com>
2023-10-17 17:36:00 +0900

    Add : 날씨 api 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+
 import "./App.css";
 import Header from "./components/Header";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,59 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Weather() {
-  return <div>hi</div>;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=37.5833&lon=127&lang=kr&appid=${apiKey}`;
+
+  const [data, setData] = useState(null); // 초기에 null로 설정
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        const weatherData = res.data;
+        setData(weatherData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  if (data === null) {
+    // 데이터가 도착하지 않았을 때
+    return <div>Loading...</div>;
+  }
+
+  console.log(data);
+
+  const {
+    name,
+    weather,
+    main: { temp, feels_like, humidity },
+    wind: { speed },
+    clouds: { all },
+  } = data;
+
+  const celsiusTemp = Math.round(((temp - 273.15) * 10) / 10);
+
+  // 날씨 아이콘 코드
+  const weatherIconCode = data.weather[0].icon;
+
+  // 아이콘 이미지 URL
+  const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
+
+  return (
+    <div>
+      <h2>오늘의 날씨:</h2>
+      <p>
+        날씨: <img src={weatherIconUrl} alt="Weather Icon" />
+      </p>
+      <p>상태: {weather[0].description}</p>
+      <p>온도: {celsiusTemp}°C</p>
+      <p>체감 온도: {Math.round(feels_like - 273.15)}°C</p>
+      <p>습도: {humidity}%</p>
+      <p>풍속: {speed} m/s</p>
+      <p>구름 양: {all}%</p>
+    </div>
+  );
 }
